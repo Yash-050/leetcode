32c947c995807f4987224db41e970a8e788ca078
Yash-050 <yashkumarsoni178@gmail.com>
2026-07-22 21:49:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (89.00%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -13,17 +13,17 @@ public:
         // Store (frequency, character)
         for (int i = 0; i < 128; i++) {
             if (freq[i] > 0) {
-                v.push_back({freq[i], (char)i});
+                v.push_back({freq[i], (char)i});//remember it handy method
             }
         }
 
         // Sort by frequency in descending order
-        sort(v.begin(), v.end(), greater<pair<int, char>>());
+        sort(v.begin(), v.end(), greater<pair<int, char>>());//used to sort the freq in pair
 
         string ans = "";
 
         for (auto i : v) {
-            for (int j = 0; j < i.first; j++) {
+            for (int j = 0; j < i.first; j++) {//printing until the freq becomes 0
                 ans.push_back(i.second);
             }
         }
