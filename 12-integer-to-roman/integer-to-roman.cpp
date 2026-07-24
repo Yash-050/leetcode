9bb18a7d9bc53fc7ae036d3b315340c0eebb6c83
Yash-050 <yashkumarsoni178@gmail.com>
2026-07-24 16:21:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (95.19%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     string intToRoman(int num) {//intresting i say
         string ans = "";
-
-        while (num >= 1000)
+        while (num!=0){
+        if (num >= 1000)
             ans += "M", num -= 1000;
-        while (num >= 900)
+        else if (num >= 900)
             ans += "CM", num -= 900;
-        while (num >= 500)
+        else if (num >= 500)
             ans += "D", num -= 500;
-        while (num >= 400)
+        else if (num >= 400)
             ans += "CD", num -= 400;
-        while (num >= 100)
+        else if (num >= 100)
             ans += "C", num -= 100;
-        while (num >= 90)
+        else if (num >= 90)
             ans += "XC", num -= 90;
-        while (num >= 50)
+        else if (num >= 50)
             ans += "L", num -= 50;
-        while (num >= 40)
+        else if (num >= 40)
             ans += "XL", num -= 40;
-        while (num >= 10)
+        else if (num >= 10)
             ans += "X", num -= 10;
-        while (num >= 9)
+        else if (num >= 9)
             ans += "IX", num -= 9;
-        while (num >= 5)
+        else if (num >= 5)
             ans += "V", num -= 5;
-        while (num >= 4)
+        else if (num >= 4)
             ans += "IV", num -= 4;
-        while (num >= 1)
+        else if (num >= 1)
             ans += "I", num -= 1;
-
+        }
         return ans;
     
 }
