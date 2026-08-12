3a6bded0a76d5abb5d26e0bf2e8ffd1ea7dc91b4
Yash-050 <yashkumarsoni178@gmail.com>
2026-08-12 23:20:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (83.18%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     int hammingWeight(int n) {//by finding the binary string
-        string s  = "";
-        while(n!=0){
-           if(n%2 == 0)s.push_back('0');
-           else s.push_back('1');
-           n = n/2;
-        }
-        reverse(s.begin(),s.end());
         int cnt = 0 ;
-        for(char c:s){
-            if(c=='1')cnt++;
+        while(n!=0){
+            if(n%2==1)cnt++;
+            n = n/2;
+            
         }
         return cnt;
     }
