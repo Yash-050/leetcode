60bdefad26ee594682385a2af906f90e37d89a4f
Yash-050 <yashkumarsoni178@gmail.com>
2026-08-19 10:56:59 +0530

    Time: 1 ms (44.44%) | Memory: 13.2 MB (87.50%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-         unordered_map<int,int>mp;
-        for(int i :nums)mp[i]++;
-        for(auto i :mp){
-            if(i.second<3){
-               return  i.first;
+        int ans = 0;
+        for(int bitidx =0 ;bitidx<32;bitidx++){//for counting of  count in each bit line 
+            int cnt =0 ;
+            for(int i =0 ;i<nums.size();i++ ){ // for comapring it to the last bit of the given number 
+                if(nums[i]&(1<<bitidx))cnt++;
+            }
+            if(cnt%3==1){//if not multiple of 3 
+                ans = ans | (1<<bitidx);
             }
         }
-        return -1;
+        return ans;
     }
 };
