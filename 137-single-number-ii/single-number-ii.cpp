20982254a81ff3949fff95ea585a052436660a0d
Yash-050 <yashkumarsoni178@gmail.com>
2026-08-20 01:15:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (66.17%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int bitidx =0 ;bitidx<32;bitidx++){//for counting of  count in each bit line 
-            int cnt =0 ;
-            for(int i =0 ;i<nums.size();i++ ){ // for comapring it to the last bit of the given number 
-                if(nums[i]&(1<<bitidx))cnt++;
-            }
-            if(cnt%3==1){//if not multiple of 3 
-                ans = ans | (1<<bitidx);
-            }
+        int ones = 0;
+        int twos = 0;
+
+        for (int num : nums) {
+            ones ^= num & ~twos;
+            twos ^= num & ~ones;
         }
-        return ans;
+
+        return ones;
     }
 };
