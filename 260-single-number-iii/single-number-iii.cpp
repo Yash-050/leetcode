ee61039058b8063a4027eede58aadc9b1012d296
Yash-050 <yashkumarsoni178@gmail.com>
2026-08-21 22:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (30.56%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int>mp;vector<int>ans;
-        for(int i :nums)mp[i]++;
-        for(auto i :mp){
-            if(i.second<2){
-                ans.push_back(i.first);
-            }
+        long long xr = 0 ;
+        for(int i =0 ;i<nums.size();i++){
+            xr = xr ^nums[i];//for finding the shifted bit value 
         }
-        return ans;
+        int rightmost = xr & (-xr);//for the rightmost set bit 
+        int b1 =0 , b2 = 0 ;
+        for(int i =0 ;i<nums.size();i++){
+            if(nums[i]&rightmost)//when the the rightmost is 1
+             b1 = b1^nums[i];
+            else 
+              b2 = b2^nums[i];
+        }
+        return {b1 , b2};
     }
 };
