0ab7c5a3116fa7ebf9b84eac4cf1fd1ece003f98
Yash-050 <yashkumarsoni178@gmail.com>
2026-09-09 00:17:27 +0530

    Time: 7 ms (41.56%) | Memory: 15.4 MB (66.80%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -2,22 +2,48 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        long long ans = 0;
+        vector<int>nse(n) , pse(n);
+        stack<int>st;
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty()&&nums[st.top()]>= nums[i])st.pop();
+            if(st.empty())nse[i] = n;
+            else nse[i] = st.top(); 
 
-        for (int i = 0; i < n; i++) {
-
-            int mn = INT_MAX;
-            int mx = INT_MIN;
-
-            for (int j = i; j < n; j++) {
-
-                mn = min(mn, nums[j]);
-                mx = max(mx, nums[j]);
-
-                ans += mx - mn;
-            }
+            st.push(i);
         }
-
-        return ans;
+        while(!st.empty())st.pop();
+        for(int i = 0 ;i<n;i++){
+            while(!st.empty()&&nums[st.top()]>nums[i])st.pop();
+            if(st.empty())pse[i] = -1;
+            else pse[i] = st.top();
+            st.push(i);
+        }
+        long long ans1 = 0 ,ans2 = 0 ; 
+        for(int i = 0 ;i<n;i++){
+            long long left = i-pse[i];
+            long long  right = nse[i] - i;
+            ans1 += 1LL*(left *right*nums[i]);
+        }
+        // nse.clear();pse.clear();
+        while(!st.empty())st.pop();
+        for(int i = n-1;i>=0;i--){
+            while(!st.empty()&&nums[st.top()]<= nums[i])st.pop();
+            if(st.empty())nse[i] = n;
+            else nse[i] = st.top();
+            st.push(i); 
+        }
+        while(!st.empty())st.pop();
+        for(int i = 0 ;i<n;i++){
+            while(!st.empty()&&nums[st.top()]<nums[i])st.pop();
+            if(st.empty())pse[i] = -1;
+            else pse[i] = st.top();
+            st.push(i);
+        }
+        for(int i = 0 ;i<n;i++){
+            long long left = i-pse[i];
+            long long right = nse[i] - i;
+            ans2 += 1LL*(left *right*nums[i]);
+        }
+        return ans2-ans1;
     }
 };
