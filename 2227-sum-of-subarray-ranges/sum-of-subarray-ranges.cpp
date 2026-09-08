ca3af5f75445644c66e0d6f896302f27ec96c655
Yash-050 <yashkumarsoni178@gmail.com>
2026-09-08 18:07:05 +0530

    Time: 18 ms (14.55%) | Memory: 14.1 MB (80.22%) - LeetSync

---
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            int mn = INT_MAX;
+            int mx = INT_MIN;
+
+            for (int j = i; j < n; j++) {
+
+                mn = min(mn, nums[j]);
+                mx = max(mx, nums[j]);
+
+                ans += mx - mn;
+            }
+        }
+
+        return ans;
+    }
+};
