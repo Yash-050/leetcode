9683976c1f3bc74ef9bfb19e497ef79fcc0fc7e6
Yash-050 <yashkumarsoni178@gmail.com>
2026-09-17 00:15:25 +0530

    Time: 40 ms (29.31%) | Memory: 97.2 MB (9.64%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -1,22 +1,19 @@
 class StockSpanner {
 public:
-    vector<int> prices;
+    stack<pair<int, int>> st;
 
     StockSpanner() {
     }
 
     int next(int price) {
-        prices.push_back(price);
+        int span = 1;
 
-        int span = 0;
-
-        for (int i = prices.size() - 1; i >= 0; i--) {
-            if (prices[i] <= price) {
-                span++;
-            } else {
-                break;
-            }
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
         }
+
+        st.push({price, span});
 
         return span;
     }
